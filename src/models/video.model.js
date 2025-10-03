d1fd57c4477eeb5c3adc1f876f6c8035d61abafc
anshsingh1032 <thansh1032@gmail.com>
2025-10-03 15:21:05 +0530

    use of multer and cloudinary for file upload

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -29,7 +29,7 @@ const videoSchema = new Schema(
         },
         isPublished:{
             type:Boolean,
-            default:TransformStreamDefaultController
+            default:true
         },
         owner:{
             type:Schema.Types.ObjectId,
