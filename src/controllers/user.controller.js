d213b15dfa4dc801a49a876e580c0749fa5242d9
anshsingh1032 <thansh1032@gmail.com>
2025-10-05 01:47:40 +0530

    fix all register controller bugs and some additonal bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -23,21 +23,32 @@ const registerUser = asyncHandler(async (req,res)=>{
     ) {
         throw new ApiError(400,"all fields are required")
     }
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or:[{ email },{ username }]
     })
     if (existedUser) {
         throw new ApiError(409,"user with this email or username already exists ")
     }
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // console.log(req.files)  
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    // console.log(avatarLocalPath);
+    
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path
+     let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
     if (!avatarLocalPath) {
         throw new ApiError(400,"avatar file is required")
     }
     const avatar = await uploadOnCloudinary(avatarLocalPath)
+    // console.log(avatar);
+    
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+    // console.log(coverImage);
+    
     if (!avatar) {
-        throw new ApiError(400,"avatar file is required")
+        throw new ApiError(400,"avatar file is required 2")
     }
     const user = await User.create({
         fullName,
@@ -48,7 +59,7 @@ const registerUser = asyncHandler(async (req,res)=>{
         username:username.toLowerCase()
     })
     const createdUser  = await User.findById(user._id).select(
-        "-password , -refreshToken"
+        "-password -refreshToken"
     )
     return res.status(201).json(
         new ApiResponse(200,createdUser,"user registered successfully")
